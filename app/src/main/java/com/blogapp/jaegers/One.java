a7000cb366651ec1ad3bc202ed3fec47691d0108
mtnjoroge <mtnjoroge@gmail.com>
2015-07-22 14:40:42 +0300

    added One to jaegers

---
+++ app/src/main/java/com/blogapp/jaegers/One.java
@@ -0,0 +1,16 @@
+/**
+ * Created by tony on 21/07/15.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.blogapp.jaegers.R;
+
+public class One extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one);
+    }
+}
