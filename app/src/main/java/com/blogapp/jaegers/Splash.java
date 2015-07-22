efb2b145bbfebc9da419cbb4bb64cf6c929e6fa2
mtnjoroge <mtnjoroge@gmail.com>
2015-07-22 14:44:21 +0300

    added splash to jaegers directory

---
+++ app/src/main/java/com/blogapp/jaegers/Splash.java
@@ -0,0 +1,16 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.blogapp.jaegers.R;
+
+/**
+ * Created by martin on 21/07/15.
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
