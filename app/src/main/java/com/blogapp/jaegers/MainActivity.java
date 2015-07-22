d9efcb25ad1503c4d87d076a17b06bfdfffe76e6
mtnjoroge <mtnjoroge@gmail.com>
2015-07-22 14:57:01 +0300

    added fb variable

--- app/src/main/java/com/blogapp/jaegers/MainActivity.java
+++ app/src/main/java/com/blogapp/jaegers/MainActivity.java
@@ -1,34 +1,25 @@
 package com.blogapp.jaegers;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+/*import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;*/
 import android.os.Bundle;
-import android.view.Menu;
+/*import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.View.OnClickListener;*/
 import android.widget.Button;
-;
+
 
 
 public class MainActivity extends Activity {
+
+    Button fb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // listens for a click and creates new function called listnr
-        OnClickListener listnr = new OnClickListener() {
-            @Override
-        //Runs whatever is in the one class
-            public void onClick(View v) {
-                Intent i = new Intent("One");
-                startActivity(i);
-            }
-        };
-        //Assignes the signup button to the listnr function
-            Button btn = (Button) findViewById(R.id.signUp);
-            btn.setOnClickListener(listnr);
+
 
         }
     }
